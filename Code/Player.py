cd95526968e3b577dbce8acf78d572db2c7cce98
AleemHussain <aleemhussain2002@gmail.com>
2025-03-13 16:35:45 +0100

    added make_move and parameters

--- Code/Player.py
+++ Code/Player.py
@@ -1,2 +1,13 @@
 class Player:
-    pass
+    """
+    This class represents a player in the game.
+    The `mode` parameter is used for the MyBot class but has no function here.
+    """
+
+    def __init__(self, name, number, mode):
+        self.name = name
+        self.number = number
+        self.mode = mode
+
+    def make_move():
+        pass
